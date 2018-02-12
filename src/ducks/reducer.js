3fae494a673aa4d1e5c2aae789e9216dbe8e941c
Sara Smith <sarasmith@Saras-MacBook-Air.local>
2018-02-12 16:45:58 -0600

    steps 1-11

--- src/ducks/reducer.js
+++ src/ducks/reducer.js
@@ -1,5 +1,96 @@
-function reducer(){
+const UPDATE_LOAN_TYPE = "UPDATE_LOAN_TYPE";
+const UPDATE_PROPERTY_TYPE = "UPDATE_PROPERTY_TYPE";
+const UPDATE_CITY = "UPDATE_CITY";
+const UPDATE_PROP = "UPDATE_PROP";
+const UPDATE_FOUND = "UPDATE_FOUND";
+const UPDATE_AGENT = "UPDATE_AGENT";
+const UPDATE_COST = "UPDATE_COST";
+const UPDATE_DOWN_PAYMENT = "UPDATE_DOWN_PAYMENT";
+const CREDIT = "CREDIT";
+const HISTORY = "HISTORY";
+const ADDRESS1 = "ADDRESS1";
+const ADDRESS2 = "ADDRESS2";
+const ADDRESS3 = "ADDRESS3";
+const FIRST_NAME = "FIRST_NAME";
+const LAST_NAME = "LAST_NAME";
+const EMAIL = "EMAIL";
 
-}
+
+export function updateLoanType (loanType) {
+    return {
+        type: UPDATE_LOAN_TYPE,
+        payload: loanType
+    };
+};
+
+export function updatePropertyType(property) {
+    return { 
+        type: UPDATE_PROPERTY_TYPE,
+        payload: property
+    };
+};
+
+export function updateCity(city) {
+    return {
+        type: UPDATE_CITY,
+        payload: city
+    };
+};
+
+export function updateProp(prop) {
+    return {
+        type: UPDATE_PROP,
+        payload: prop
+    };
+};
+
+export function updateFound(found) {
+    return {
+        type: UPDATE_FOUND,
+        payload: found
+    };
+};
+
+const initialState = {
+    loanType: 'Home Purchase',
+    propertyType: 'Single Family Home',
+    city: '',
+    propToBeUsedOn: '',
+    found: false,
+    realEstateAgent: "false",
+    cost: 0,
+    downPayment: 0,
+    credit: '',
+    history: '',
+    addressOne: '',
+    addressTwo: '',
+    addressThree: '',
+    firstName: '',
+    lastName: '',
+    email: ''
+};
+
+
+function reducer( state = initialState, action ){
+    switch(action.type) {
+        case UPDATE_LOAN_TYPE: 
+            return Object.assign( {}, state, {loanType: action.payload});
+
+        case UPDATE_PROPERTY_TYPE:
+            return Object.assign( {}, state, {propertyType: action.payload});
+
+        case UPDATE_CITY:
+            return Object.assign( {}, state, {city: action.payload}); 
+            
+        case UPDATE_PROP:
+            return Object.assign( {}, state, {propToBeUsedOn: action.payload});
+
+        case UPDATE_FOUND:
+            return Object.assign( {}, state, {found: action.payload});
+
+        default: return state;
+    };
+};
+
 
 export default reducer;
